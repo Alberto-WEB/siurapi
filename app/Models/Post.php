4fff364b5c7a69193e134ff6a32506dc61877437
Carlos Guzman <alberto.guzman.220793@gmail.com>
2021-10-02 17:02:24 -0500

    add relations to models

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -11,4 +11,25 @@ class Post extends Model
 
     const BORRADOR = 1;
     const PUBLICADO = 2;
+
+
+     //relacion uno a muchos inversa
+     public function user(){
+        $this->belongsTo(User::class);
+    }
+
+    public function category(){
+        $this->belongsTo(Category::class);
+    }
+
+    //relacion muchos a muchos
+    public function tags(){
+        $this->belongsToMany(Tag::class);
+    }
+
+
+    //relacion uno a muchos poliformica con el modelo image
+    public function images(){
+        return $this->morphMany(Image::class, 'imageable');
+    }
 }
