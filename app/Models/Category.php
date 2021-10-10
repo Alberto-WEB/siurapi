72a53a5037a089c5a24a65415600841be879527e
Carlos Guzman <alberto.guzman.220793@gmail.com>
2021-10-09 20:05:56 -0500

    finisd¿h endpoints categories

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -9,6 +9,8 @@ class Category extends Model
 {
     use HasFactory;
 
+    protected $fillable = ['name', 'slug'];
+
      //relacion uno a muchos
      public function posts(){
         return $this->hasMany(Post::class);
