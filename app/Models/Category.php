10a3c5d9ea7fef3f09dedb27ba7212cfab793525
Carlos Guzman <alberto.guzman.220793@gmail.com>
2021-10-10 14:47:00 -0500

    relations model categories

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -11,8 +12,29 @@ class Category extends Model
 
     protected $fillable = ['name', 'slug'];
 
+    protected $allowIcluded = ['posts', 'posts.user'];
+
      //relacion uno a muchos
      public function posts(){
         return $this->hasMany(Post::class);
+    }
+
+    public function scopeIncluded(Builder $query){
+
+        if (empty($this->allowIcluded) || empty(request('included'))) {
+            return;
+        }
+
+        $relations = explode(',', request('included'));
+
+        $allowIcluded = collect($this->allowIcluded);
+
+        foreach ($relations as $key => $relationship) {
+            if (!$allowIcluded->contains($relationship)) {
+                unset($relations[$key]);
+            }
+        }
+
+        $query->with($relations);
     }
 }
