a6170db62476c456c4bc1516e6104f1235def6d1
Carlos Guzman <alberto.guzman.220793@gmail.com>
2021-10-16 21:18:18 -0500

    filter data and finish scopes

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -18,7 +18,7 @@ class CategoryController extends Controller
         $categories = Category::included()
                         ->filter()
                         ->sort()
-                        ->get();
+                        ->GetOrPaginate();
 
         return $categories;
     }
--- app/Models/Category.php
+++ app/Models/Category.php
@@ -79,4 +79,18 @@ class Category extends Model
             }
         }
     }
+
+
+    public function scopeGetOrPaginate(Builder $query){
+        if (request('perPage')) {
+            $perPage = intval(request('perPage'));
+
+        if ($perPage) {
+            return $query->paginate($perPage);
+        }
+
+        }
+
+        return $query->get();
+    }
 }
